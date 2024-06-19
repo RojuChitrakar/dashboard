ccb054651630754f467fc9b03eb0344e75fb09fb
Roju Chitrakar <rojuchit00@gamail.com>
2024-06-19 21:52:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function hello(){
+    const popup = document.getElementById("popup");
+    
+    popup.style.display='block';
+    }
+function closee(){
+    const popup = document.getElementById("popup");
+    popup.style.display='none';
+}
